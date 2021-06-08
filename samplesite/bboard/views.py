ab4e7d292ed2720e5a594a163ae99cbf8525f1bf
User <kd200681@mail.ru>
2021-06-08 19:39:17 +0300

    add bboard template, admin

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Bb
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    context = {'bbs': bbs}
+    return render(request, 'bboard/index.html', context)
+
